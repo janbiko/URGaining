fa5f4f1deb9f51c4ebe077359ecd7972df098e7e
Jannik Bikowski <janbiko@gmx.de>
2017-09-13 14:21:07 +0200

    Implemented main menu base

--- app/src/main/java/janbiko/urgaining/MainActivity.java
+++ app/src/main/java/janbiko/urgaining/MainActivity.java
@@ -1,13 +1,71 @@
 package janbiko.urgaining;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button workoutsButton;
+    private Button progressButton;
+    private Button settingsButton;
+    private Button quitButton;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+    }
+
+    private void initUI() {
+        initButtons();
+        initTextViews();
+    }
+
+    private void initTextViews() {
+        totalTextView = (TextView) findViewById(R.id.total_textview);
+    }
+
+    private void initButtons() {
+        workoutsButton = (Button) findViewById(R.id.workouts_button);
+        workoutsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, WorkoutsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        progressButton = (Button) findViewById(R.id.progress_button);
+        progressButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Progress", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        settingsButton = (Button) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        quitButton = (Button) findViewById(R.id.quit_button);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAndRemoveTask();
+            }
+        });
     }
 }
---
+++ app/src/main/java/janbiko/urgaining/WorkoutsActivity.java
@@ -0,0 +1,19 @@
+package janbiko.urgaining;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jannik on 13.09.2017.
+ */
+
+public class WorkoutsActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts);
+
+    }
+
+}
