09be172d1e00b640723ac4b4c4b40537a07b55d7
Jannik Bikowski <janbiko@gmx.de>
2017-09-14 23:03:17 +0200

    Workouts from db are now displayed in a ListView

--- app/src/main/java/janbiko/urgaining/WorkoutsActivity.java
+++ app/src/main/java/janbiko/urgaining/WorkoutsActivity.java
@@ -5,6 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jannik on 13.09.2017.
@@ -15,6 +21,10 @@ public class WorkoutsActivity extends Activity
 
     private FloatingActionButton addRoutineButton;
     private WorkoutsDatabase workoutsDB;
+    private ListView workoutNamesList;
+
+    private ArrayList<String> listItems = new ArrayList<>();
+    private ArrayAdapter<String> listAdapter;
 
 
     @Override
@@ -33,6 +43,51 @@ public class WorkoutsActivity extends Activity
 
     private void initUI() {
         initButtons();
+        initListViews();
+    }
+
+    private void initListViews() {
+        workoutNamesList = (ListView) findViewById(R.id.workout_names_list);
+        workoutNamesList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                removeWorkoutAtPosition(position);
+                return true;
+            }
+        });
+        workoutNamesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getApplicationContext(), "Show " + listItems.get(position) +
+                        " workout.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        listAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item,
+                listItems);
+        workoutNamesList.setAdapter(listAdapter);
+        fillListView();
+    }
+
+    private void removeWorkoutAtPosition(int position) {
+        if (listItems.get(position) != null) {
+            workoutsDB.removeWorkoutItem(listItems.get(position));
+            refreshArrayList();
+        }
+    }
+
+    private void refreshArrayList(){
+        ArrayList tempList = workoutsDB.getAllWorkoutItems();
+        listItems.clear();
+        listItems.addAll(tempList);
+        listAdapter.notifyDataSetChanged();
+    }
+
+    private void fillListView() {
+        listItems.addAll(workoutsDB.getAllWorkoutItems());
+        listAdapter.notifyDataSetChanged();
     }
 
     private void initButtons() {
